bb77ae9fac19d96d7a9ac0fe1017ce8bebadf918
James <jegpub@gmail.com>
2017-02-10 17:01:09 -0600

    Finis the routes

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -8,6 +8,20 @@ class PostsIndex extends Component {
   componentWillMount() {
     this.props.fetchPosts();
   }
+
+  renderPosts() {
+    return this.props.posts.map((post) => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          <Link to={`posts/${post.id}`}>
+            <span className="pull-xs-right">{post.categories}</span>
+            <strong>{post.title}</strong>
+          </Link>
+        </li>
+      );
+    });
+  }
+
   render() {
     return (
       <div>
@@ -16,7 +30,10 @@ class PostsIndex extends Component {
             Add a Post
           </Link>
         </div>
-        List of blogs
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
@@ -26,4 +43,8 @@ class PostsIndex extends Component {
 //   return bindActionCreators({ fetchPosts }, dispatch);
 // }
 
-export default connect(null, { fetchPosts })(PostsIndex);
+function mapStateToProps(state) {
+  return { posts: state.posts.all };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
